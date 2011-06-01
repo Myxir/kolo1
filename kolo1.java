71b643c1db650c915168f100ea2d00002c1546b6
Lukasz Piotrowski <myxir20@gmail.com>
2011-06-01 14:43:50 +0200

    9 zadanie

--- kolo1.java
+++ kolo1.java
@@ -1,9 +1,19 @@
 //Lukasz Piotrowski
 
-class Kolo1 {
-  public static void main(String[] args) {
-    System.out.println("I love git!");
-  }
+#include <iostream>
 
+using namespace std;
+
+void pytanie() {
+  cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;
+  cout<<"A1) ..."<<endl;
+  cout<<"Q2) Co sie zmienilo?"<<endl;
+  cout<<"A2) ..."<<endl;
 }
+
+ int main(int argc, char **argv) {
+  pytanie();
+   return 0;
+ }
+ 
 
